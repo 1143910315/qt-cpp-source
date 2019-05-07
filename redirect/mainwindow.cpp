31745b510f0728009792e70d053f342e9c0b8e31
林嘉豪 <1143910315@qq.com>
2019-05-07 17:41:32 +0800

    生成9个随机颜色的块

--- redirect/mainwindow.cpp
+++ redirect/mainwindow.cpp
@@ -2,12 +2,25 @@
 #include "ui_mainwindow.h"
 
 #include <QMimeData>
+#include <QRandomGenerator>
+#include <QStyle>
+#include <QTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QRandomGenerator random=QRandomGenerator::securelySeeded();
+    ui->graphicsView->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_2->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_3->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_4->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_5->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_6->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_7->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_8->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
+    ui->graphicsView_9->setStyleSheet(QString::asprintf("background: rgb(%d,%d,%d)",random.bounded(256),random.bounded(256),random.bounded(256)));
 }
 
 MainWindow::~MainWindow()
@@ -41,9 +54,7 @@ void MainWindow::dragEnterEvent(QDragEnterEvent *event)
     }
     ui->textEdit->append(str);
     event->accept();
-
 }
-
 
 
 
